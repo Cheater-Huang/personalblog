667c100fcb5815c74dc306b4c19a7924a885d535
Cheater-Huang <3395126648@qq.com>
2026-06-23 20:43:24 +0800

    'update'

--- src/components/WelcomeAnimation.tsx
+++ src/components/WelcomeAnimation.tsx
@@ -103,7 +103,7 @@ export default function WelcomeAnimation() {
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.5, duration: 1 }}
           >
-            我觉得天堂还有很远，所以我为自己打造一个乌托邦
+            我觉得我离天堂还有很远，所以我为自己打造一个乌托邦
           </motion.h1>
 
           <motion.p
